3b12ae22fb8b911db137d982adeeb2a28814aed0
Loaxn <chalachloana@gmail.com>
2025-04-08 22:49:53 +0200

    patrol enemy done, still have to fix stuff

---
+++ Assets/Script/Enemy/EnnemyController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnnemyController : MonoBehaviour
+{
+
+    [Header("Patrol")]
+    [SerializeField] private Transform wayPoints;
+    private int currentWaypoint;
+
+    [Header("Components")]
+    NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance <= 0.2f)
+        {
+            currentWaypoint++;
+            if (currentWaypoint >= wayPoints.childCount)
+            {
+                currentWaypoint = 0;
+            }
+            agent.SetDestination(wayPoints.GetChild(currentWaypoint).position);
+        }
+    }
+}
